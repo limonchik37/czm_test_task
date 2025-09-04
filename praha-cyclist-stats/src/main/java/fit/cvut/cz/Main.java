4851b7bdd1025f3da8d176810ac261ba92f29e9b
limonchik37 <ffchanel123@gmail.com>
2025-09-04 12:28:32 +0200

    some edit in main

--- praha-cyclist-stats/src/main/java/fit/cvut/cz/Main.java
+++ praha-cyclist-stats/src/main/java/fit/cvut/cz/Main.java
@@ -1,34 +1,31 @@
 package fit.cvut.cz;
 
-import fit.cvut.cz.measurement.Measurement;
+import fit.cvut.cz.console.handler.ConsoleHandler;
+import fit.cvut.cz.console.handler.Handler;
+import fit.cvut.cz.exporter.implementation.JsonExporter;
+import fit.cvut.cz.exporter.implementation.XmlExporter;
+import fit.cvut.cz.facade.ReportFacade;
+import fit.cvut.cz.facade.ReportFacadeImpl;
 import fit.cvut.cz.reader.CsvMeasurementReader;
 import fit.cvut.cz.reader.MeasurementReader;
+import fit.cvut.cz.statistics.implementations.AvgPerDayStatistic;
+import fit.cvut.cz.statistics.implementations.DayMaxStatistic;
+import fit.cvut.cz.statistics.implementations.PopularHourStatistic;
+import fit.cvut.cz.statistics.implementations.PopularSegmentStatistic;
 
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Path path = resourcePath("bc-2020-q1.csv");
-
         MeasurementReader reader = new CsvMeasurementReader();
-        List<Measurement> data = reader.read(path);
+        var stats = List.of(
+                new AvgPerDayStatistic(), new DayMaxStatistic(),
+                new PopularHourStatistic(), new PopularSegmentStatistic()
+        );
+        var exporters = List.of(new JsonExporter(), new XmlExporter());
 
-        System.out.println("Rows read: " + data.size());
-        for (int i = 0; i < Math.min(10, data.size()); i++) {
-            var m = data.get(i);
-            System.out.printf("%s -> %s | from=%s | to=%s | val=%d | date=%s | hour=%d%n",
-                    m.segment().fromId(), m.segment().toId(),
-                    m.measuredFrom(), m.measuredTo(), m.value(),
-                    m.dateUTC(), m.hourUTC());
-        }
-    }
-
-    private static Path resourcePath(String fileName) throws URISyntaxException {
-        var url = Main.class.getClassLoader().getResource(fileName);
-        if (url == null) throw new IllegalArgumentException("Resource not found: " + fileName);
-        return Paths.get(url.toURI());
+        ReportFacade facade = new ReportFacadeImpl(reader, stats, exporters);
+        Handler handler = new ConsoleHandler(facade);
+        handler.run();
     }
 }
